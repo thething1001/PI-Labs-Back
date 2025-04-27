812a3a9825cd3eefed90b4c419df3a04cff63b59
Bohdan Hamela <bohdan.hamela@gmail.com>
2025-04-27 19:38:49 +0300

    Lab03 - Refine error handling

--- apache/src/gateways/students_gateway.php
+++ apache/src/gateways/students_gateway.php
@@ -1,107 +1,137 @@
 <?php
-
 class StudentGateway
 {
-  private PDO $conn;
+    private PDO $conn;
 
-  public function __construct(Database $database)
-  {
-    $this->conn = $database->getConnection();
-  }
-
-  public function getAll(): array
-  {
-    $sql = "SELECT *
-            FROM cms_schema.students
-            ORDER BY id ASC";
-
-    $stmt = $this->conn->query($sql);
-
-    $data = [];
-    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-      $data[] = $row;
+    public function __construct(Database $database)
+    {
+        $this->conn = $database->getConnection();
     }
 
-    return $data;
-  }
+    public function getAll(): array
+    {
+        $sql = "SELECT *
+                FROM cms_schema.students
+                ORDER BY id ASC";
 
-  public function create(array $data): string
-  {
-    $sql = "INSERT INTO cms_schema.students (group_name, first_name, last_name, gender, birthday, status, email, password)
-            VALUES (:group_name, :first_name, :last_name, :gender, :birthday, :status, :email, :password)";
+        $stmt = $this->conn->query($sql);
 
-    $stmt = $this->conn->prepare($sql);
+        $data = [];
+        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            $data[] = $row;
+        }
 
-    $email = "{$data['first_name']}.{$data['last_name']}.{$data['group_name']}@cms.com";
+        return $data;
+    }
 
-    $stmt->bindValue(":group_name", $data["group_name"], PDO::PARAM_STR);
-    $stmt->bindValue(":first_name", $data["first_name"], PDO::PARAM_STR);
-    $stmt->bindValue(":last_name", $data["last_name"], PDO::PARAM_STR);
-    $stmt->bindValue(":gender", $data["gender"], PDO::PARAM_STR);
-    $stmt->bindValue(":birthday", $data["birthday"], PDO::PARAM_STR);
-    $stmt->bindValue(":status", isset($data["status"]) ? (bool) $data["status"] : false, PDO::PARAM_BOOL);
-    $stmt->bindValue(":email", $email, PDO::PARAM_STR);
-    $stmt->bindValue(":password", $data["birthday"], PDO::PARAM_STR);
+    public function create(array $data): string
+    {
+        $sql = "INSERT INTO cms_schema.students (group_name, first_name, last_name, gender, birthday, status, email, password)
+                VALUES (:group_name, :first_name, :last_name, :gender, :birthday, :status, :email, :password)";
 
-    $stmt->execute();
+        $stmt = $this->conn->prepare($sql);
 
-    return $this->conn->lastInsertId();
-  }
+        $email = "{$data['first_name']}.{$data['last_name']}.{$data['group_name']}@cms.com";
 
-  public function get(string $id) : array | false
-  {
-    $sql = "SELECT * FROM cms_schema.students WHERE id = :id";
+        $stmt->bindValue(":group_name", $data["group_name"], PDO::PARAM_STR);
+        $stmt->bindValue(":first_name", $data["first_name"], PDO::PARAM_STR);
+        $stmt->bindValue(":last_name", $data["last_name"], PDO::PARAM_STR);
+        $stmt->bindValue(":gender", $data["gender"], PDO::PARAM_STR);
+        $stmt->bindValue(":birthday", $data["birthday"], PDO::PARAM_STR);
+        $stmt->bindValue(":status", isset($data["status"]) ? (bool) $data["status"] : false, PDO::PARAM_BOOL);
+        $stmt->bindValue(":email", $email, PDO::PARAM_STR);
+        $stmt->bindValue(":password", $data["birthday"], PDO::PARAM_STR);
 
-    $stmt = $this->conn->prepare($sql);
-    $stmt->bindValue(":id", $id, PDO::PARAM_INT);
+        $stmt->execute();
 
-    $stmt->execute();
+        return $this->conn->lastInsertId();
+    }
 
-    $data = $stmt->fetch(PDO::FETCH_ASSOC);
+    public function get(string $id): array | false
+    {
+        $sql = "SELECT * FROM cms_schema.students WHERE id = :id";
 
-    return $data;
-  }
+        $stmt = $this->conn->prepare($sql);
+        $stmt->bindValue(":id", $id, PDO::PARAM_INT);
 
-  public function update(string $id, array $new): int {
-    $sql = "UPDATE cms_schema.students 
-    SET group_name = :group_name, first_name = :first_name, last_name = :last_name, 
-        gender = :gender, birthday = :birthday, status = :status, email = :email,
-        password = :password WHERE id=:id";
+        $stmt->execute();
 
-    $stmt = $this->conn->prepare($sql);
+        $data = $stmt->fetch(PDO::FETCH_ASSOC);
 
-    $email = "{$new['first_name']}.{$new['last_name']}.{$new['group_name']}@cms.com";
+        return $data;
+    }
 
-    $stmt->bindValue(":group_name", $new["group_name"], PDO::PARAM_STR);
-    $stmt->bindValue(":first_name", $new["first_name"], PDO::PARAM_STR);
-    $stmt->bindValue(":last_name",  $new["last_name"], PDO::PARAM_STR);
-    $stmt->bindValue(":gender",    $new["gender"], PDO::PARAM_STR);
-    $stmt->bindValue(":birthday",  $new["birthday"], PDO::PARAM_STR);
-    $stmt->bindValue(":status",  isset($new["status"]) ? (bool) $new["status"] : false, PDO::PARAM_BOOL);
-    $stmt->bindValue(":email", $email, PDO::PARAM_STR);
-    $stmt->bindValue(":password", $new["birthday"], PDO::PARAM_STR);
-    $stmt->bindValue(":id", $id, PDO::PARAM_INT);
+    public function update(string $id, array $new): int
+    {
+        $sql = "UPDATE cms_schema.students 
+                SET group_name = :group_name, first_name = :first_name, last_name = :last_name, 
+                    gender = :gender, birthday = :birthday, status = :status, email = :email,
+                    password = :password WHERE id = :id";
 
-    $stmt->execute();
-    return $id;
-  }
+        $stmt = $this->conn->prepare($sql);
 
-  public function deleteByID(string $id): int {
-    $sql = "DELETE FROM cms_schema.students WHERE id = :id";
+        $email = "{$new['first_name']}.{$new['last_name']}.{$new['group_name']}@cms.com";
 
-    $stmt = $this->conn->prepare($sql);
-    $stmt->bindValue(":id", $id, PDO::PARAM_INT);
+        $stmt->bindValue(":group_name", $new["group_name"], PDO::PARAM_STR);
+        $stmt->bindValue(":first_name", $new["first_name"], PDO::PARAM_STR);
+        $stmt->bindValue(":last_name", $new["last_name"], PDO::PARAM_STR);
+        $stmt->bindValue(":gender", $new["gender"], PDO::PARAM_STR);
+        $stmt->bindValue(":birthday", $new["birthday"], PDO::PARAM_STR);
+        $stmt->bindValue(":status", isset($new["status"]) ? (bool) $new["status"] : false, PDO::PARAM_BOOL);
+        $stmt->bindValue(":email", $email, PDO::PARAM_STR);
+        $stmt->bindValue(":password", $new["birthday"], PDO::PARAM_STR);
+        $stmt->bindValue(":id", $id, PDO::PARAM_INT);
 
-    $stmt->execute();
-    return $stmt->rowCount();
-  }
+        $stmt->execute();
+        return $id;
+    }
 
-  public function deleteSeveral(string $ids): int {
-    
-    $sql = "DELETE FROM cms_schema.students WHERE id IN ($ids)";
-    $stmt = $this->conn->prepare($sql);
+    public function deleteByID(string $id): int
+    {
+        $sql = "DELETE FROM cms_schema.students WHERE id = :id";
 
-    $stmt->execute();
-    return $stmt->rowCount();
-  }
+        $stmt = $this->conn->prepare($sql);
+        $stmt->bindValue(":id", $id, PDO::PARAM_INT);
+
+        $stmt->execute();
+        return $stmt->rowCount();
+    }
+
+    public function deleteSeveral(string $ids): int
+    {
+        $sql = "DELETE FROM cms_schema.students WHERE id IN ($ids)";
+        $stmt = $this->conn->prepare($sql);
+
+        $stmt->execute();
+        return $stmt->rowCount();
+    }
+
+    public function findByUniqueFields(
+        string $first_name,
+        string $last_name,
+        string $birthday,
+        string $gender,
+        string $group_name
+    ): array | false
+    {
+        $sql = "SELECT * FROM cms_schema.students 
+                WHERE first_name = :first_name 
+                  AND last_name = :last_name 
+                  AND birthday = :birthday 
+                  AND gender = :gender 
+                  AND group_name = :group_name 
+                LIMIT 1";
+
+        $stmt = $this->conn->prepare($sql);
+
+        $stmt->bindValue(":first_name", $first_name, PDO::PARAM_STR);
+        $stmt->bindValue(":last_name", $last_name, PDO::PARAM_STR);
+        $stmt->bindValue(":birthday", $birthday, PDO::PARAM_STR);
+        $stmt->bindValue(":gender", $gender, PDO::PARAM_STR);
+        $stmt->bindValue(":group_name", $group_name, PDO::PARAM_STR);
+
+        $stmt->execute();
+
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
 }
